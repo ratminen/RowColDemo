e038a9d3bcf594d28b1299c534c5c80b6fdcd978
ratminen <ghgpoklonik16@gmail.com>
2025-09-23 16:20:15 +0700

    SpaceEvenly

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -42,7 +42,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(horizontalArrangement = Arrangement.SpaceBetween,
+    Row(horizontalArrangement = Arrangement.SpaceEvenly,
         modifier = modifier.width(1000.dp)) {
         TextCell("1")
         TextCell("2")
