8aad7cc98a36b145ac41375da3397f402d082b2f
ratminen <ghgpoklonik16@gmail.com>
2025-09-23 14:33:56 +0700

    5. Different distance

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -42,8 +42,8 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Column(verticalArrangement = Arrangement.Bottom,
-        modifier = modifier.height(400.dp)) {
+    Row(horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier.width(1000.dp)) {
         TextCell("1")
         TextCell("2")
         TextCell("3")
